47b827bb49c84c8da62656d707961e36961192a7
qxan <mehmet.enfa@gmail.com>
2023-03-03 16:14:17 +0300

    createElement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { createElement } from "react";
+
 import "./style.scss";
 
 function App() {
+
+  const todos = ['todo1', 'todo2', 'todo3']
+  const h1 = createElement('h1', null, 'mehmetenfa.com')
+  const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
+  return createElement('main', {
+    className: 'test',
+    id: 'main',
+  }, h1, ul)
+
   return (
     <main>
       <h1>mehmetenfa.com</h1>
+      <ul>
+        {todos.map(todo => (
+          <li>
+            {todo}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
