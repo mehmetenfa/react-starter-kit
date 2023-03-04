eebc528e5422a4da0634ea7314fe52399d46f7b5
qxan <mehmet.enfa@gmail.com>
2023-03-04 19:42:39 +0300

    button createElement

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import { createElement } from "react";
 
 import "./style.scss";
 
+function Button(props){
+  return <button>{props.text}</button>
+}
+
 function App() {
 
   const todos = ['todo1', 'todo2', 'todo3']
-  // const h1 = createElement('h1', null, 'mehmetenfa.com')
-  // const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
-  // return createElement('main', {
-  //   className: 'test',
-  //   id: 'main',
-  // }, h1, ul)
+  const h1 = createElement('h1', null, 'mehmetenfa.com')
+  const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
+  const button = createElement(Button, {
+    text: 'Buton texti'
+  })
+  return createElement('main', {
+    className: 'test',
+    id: 'main',
+  }, h1, ul, button)
 
   const name = 'Mehmet Enfa'
 
   return (
     <main>
+      <Button text='Merhaba Button' />
       <h1 tabIndex='3' style={{color: 'red', backgroundColor: 'yellow'}}>mehmetenfa.com</h1>
       <label htmlFor="search" tabIndex='2' onClick={() => alert('merhaba')}>Arama</label>
       <input type='text' id='search' tabIndex='1' />
