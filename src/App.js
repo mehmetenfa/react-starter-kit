b21ca497542dffe07174509376e14af98dab4ee5
qxan <mehmet.enfa@gmail.com>
2023-03-04 19:08:17 +0300

    .

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import "./style.scss";
 function App() {
 
   const todos = ['todo1', 'todo2', 'todo3']
-  const h1 = createElement('h1', null, 'mehmetenfa.com')
-  const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
-  return createElement('main', {
-    className: 'test',
-    id: 'main',
-  }, h1, ul)
+  // const h1 = createElement('h1', null, 'mehmetenfa.com')
+  // const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
+  // return createElement('main', {
+  //   className: 'test',
+  //   id: 'main',
+  // }, h1, ul)
+
+  const name = 'Mehmet Enfa'
 
   return (
     <main>
-      <h1>mehmetenfa.com</h1>
+      <h1 tabIndex='3' style={{color: 'red', backgroundColor: 'yellow'}}>mehmetenfa.com</h1>
+      <label htmlFor="search" tabIndex='2' onClick={() => alert('merhaba')}>Arama</label>
+      <input type='text' id='search' tabIndex='1' />
       <ul>
-        {todos.map(todo => (
-          <li>
+        {name} Vakitsayan
+        {todos.map((todo, index) => (
+          <li key={index}>
             {todo}
           </li>
         ))}
